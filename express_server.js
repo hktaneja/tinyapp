bf25d1bfd84d43b4fcfd088bd1a2051340fcb362
hktaneja <hktaneja2020@gmail.com>
2023-07-24 20:11:02 -0500

    Added URL Submission Form

--- express_server.js
+++ express_server.js
@@ -9,17 +9,33 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//Express library to translate or parse the body. 
+app.use(express.urlencoded({ extended: true }));
+
+//Generate a Random Short URL ID
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < 6; i++) {
+    //get a random number in the range of [0, characters.length] and rounds down a given number to the nearest integer
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
+
+// route definitions 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };  
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -28,9 +44,12 @@ app.get("/urls/:id", (req, res) => {
 });
 
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// handle POST request
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
